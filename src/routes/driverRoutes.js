b8485fc60638f316cbb9f12a7f8d7ca84f0d3845
ObodoakorDavid <obodoakordavid@outlook.com>
2024-06-14 14:50:50 +0100

    removed add driver from normal route

--- src/routes/driverRoutes.js
+++ src/routes/driverRoutes.js
@@ -1,15 +1,11 @@
 import express from "express";
 import methodNotAllowed from "../utils/methodNotAllowed.js";
-import {
-  addDriver,
-  getAllDrivers,
-  getDriver,
-} from "../controllers/driverController.js";
+import { getAllDrivers, getDriver } from "../controllers/driverController.js";
 import { validateParamId } from "../validators/IdValidator.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllDrivers).post(addDriver).all(methodNotAllowed);
+router.route("/").get(getAllDrivers).all(methodNotAllowed);
 router
   .route("/:driverId")
   .get(validateParamId("driverId"), getDriver)
